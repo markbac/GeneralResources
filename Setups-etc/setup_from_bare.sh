412881ce2613ec03a05a05df7aa057a48ba94292
markbac <109081587+markbac@users.noreply.github.com>
2023-01-07 15:07:45 +0000

    changes from testing

--- Setups-etc/setup_from_bare.sh
+++ Setups-etc/setup_from_bare.sh
@@ -14,12 +14,26 @@ echo $ProvidedPasword | sudo -S apt -y upgrade
 echo "apt install packages"
 # for Ubuntu
 echo $ProvidedPasword | sudo -S apt-get -y install build-essential procps curl file git \
-    python3 inetutils-traceroute net-tools wireless-tools graphviz python3-pip ctop \
-    libgraphviz-dev x11-apps x11-apps neofetch wget htop glances bat tree gccgo-go \
-    nmon atop nodejs bashtop gpg inxi unzip neofetch screenfetch httrack fd-find \
-    wkhtmltopdf default-jre exa debget apt-transport-https ca-certificates gnupg \ 
+    python3 inetutils-traceroute net-tools wireless-tools graphviz python3-pip  \
+    libgraphviz-dev x11-apps x11-apps  neofetch wget htop  bat tree gccgo-go \
+    nmon atop nodejs  gpg inxi unzip neofetch screenfetch httrack fd-find \
+    wkhtmltopdf default-jre exa debian-goodies apt-transport-https ca-certificates gnupg \
     snapd libssl-dev nedit gimp emacs dolphin  gnome-system-monitor flatpak
     
+# ctop glances bashop debget replaced by debian-goodies
+
+echo "set up ssh"
+ssh-keygen -t ed25519 -C "mark.bacon@landisgyr.com"
+eval "$(ssh-agent -s)"
+ssh-add ~/.ssh/id_ed25519
+# cat ~/.ssh/id_ed25519.pub
+# add key to the git server e.g. github gitlabs
+# ssh -T git@github.com
+
+echo "config git"
+git config --global user.name "Mark Bacon"
+git config --global user.email "mark@bacon.me.uk"
+
 
 ##
 ## snapd
@@ -34,24 +48,24 @@ echo $ProvidedPasword | sudo -S apt-get -y install build-essential procps curl f
 #echo $ProvidedPasword | sudo -S echo "[boot]" >>  /etc/wsl.conf
 #echo $ProvidedPasword | sudo -S echo "systemd=true" >>  /etc/wsl.conf
 
-#sudo snap install core
-#sudo snap install bashtop
-#sudo snap install nutty
-#sudo snap install krita 
-#sudo snap install standard-notes
-#sudo snap install wekan 
-#sudo snap install onefetch 
-#sudo snap install fast 
-#sudo snap install snap-store
-#sudo snap install imagenes
-#sudo snap install sublime-text
-#sudo snap install pycharm-community --classic
-#sudo snap install sublime-text --classic
-#sudo snap install notepadqq
-#sudo snap install bottom
-#sudo snap install mqtt-explorer
-#sudo snap install mosquitto
-#sudo snap install redis
+sudo snap install core
+sudo snap install bashtop
+sudo snap install nutty
+sudo snap install krita 
+sudo snap install standard-notes
+sudo snap install wekan 
+sudo snap install onefetch 
+sudo snap install fast 
+sudo snap install snap-store
+sudo snap install imagenes
+sudo snap install sublime-text
+sudo snap install pycharm-community --classic
+sudo snap install sublime-text --classic
+sudo snap install notepadqq
+sudo snap install bottom
+sudo snap install mqtt-explorer
+sudo snap install mosquitto
+sudo snap install redis
 
 
 #install bashtop from source
@@ -60,13 +74,15 @@ echo $ProvidedPasword | sudo -S apt-get -y install build-essential procps curl f
 #echo $ProvidedPasword | sudo -S make install
 
 echo "Install rust"
-wget https://sh.rustup.rs
+curl https://sh.rustup.rs -sSf | sh
 
-cargo install cargo-update
-cargo install-update -a
+source $HOME/.cargo/env
+
+##cargo install cargo-update
+##cargo install-update -a
 
 echo "Install rust apps"
-cargo install  du-dust tokei bottom gitui webget
+##cargo install  du-dust tokei bottom gitui webget
 
 echo "install pfetch"
 wget https://github.com/dylanaraps/pfetch/archive/master.zip
@@ -76,26 +92,26 @@ rm -rf master.zip pfetch-master
 
 flatpak install --user com.jetpackduba.Gitnuro
 
+#brew onyly for non ARM (i.e not raspberry pi)
 
 #echo "Install Brew"
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-echo '# Set PATH, MANPATH, etc., for Homebrew.' >> /home/mbacon/.profile
-echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> /home/mbacon/.profile
-eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+##/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+##echo '# Set PATH, MANPATH, etc., for Homebrew.' >> /home/mbacon/.profile
+##echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> /home/mbacon/.profile
+##eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 echo "brew install"
-brew install git gcc ctop pstree lazydocker macchina xplr rust jandedobbeleer/oh-my-posh/oh-my-posh  ##tokei bottom gitui exa # doesnt work
+##brew install git gcc ctop pstree lazydocker macchina xplr rust jandedobbeleer/oh-my-posh/oh-my-posh  ##tokei bottom gitui exa # doesnt work
 ##brew tap tgotwig/linux-dust && brew install dust # doesnt work
-brew install git 
-brew install gcc 
-brew install ctop 
-brew install pstree 
-brew install lazydocker 
-brew install macchina 
-brew install xplr 
-#brew install rust
-brew install jandedobbeleer/oh-my-posh/oh-my-posh
-brew upgrade
-brew install helix
+##brew install git 
+##brew install gcc 
+##brew install ctop 
+##brew install pstree 
+##brew install lazydocker 
+##brew install macchina 
+##brew install xplr 
+##brew install jandedobbeleer/oh-my-posh/oh-my-posh
+##brew upgrade
+##brew install helix
 
 # add teh following to you ~/.bashrc
 PATH=$PATH:/home/mbacon/.cargo/bin
@@ -145,7 +161,10 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+echo "nvm install"
+source ~/.bashrc
 nvm install --lts
+source ~/.bashrc
 # asyncapi tools
 echo "install syncapi tools"
 npm install -g @asyncapi/cli
@@ -198,14 +217,15 @@ if [[ ! -d "$fonts_dir" ]]; then
     mkdir -p "$fonts_dir"
 fi
 
-for font in "${fonts[@]}"; do
-    zip_file="${font}.zip"
-    download_url="https://github.com/ryanoasis/nerd-fonts/releases/download/v${version}/${zip_file}"
-    echo "Downloading $download_url"
-    wget "$download_url"
-    unzip "$zip_file" -d "$fonts_dir"
-    rm "$zip_file"
-done
+# this works so commeneted out so we dont try again
+#for font in "${fonts[@]}"; do
+#    zip_file="${font}.zip"
+#    download_url="https://github.com/ryanoasis/nerd-fonts/releases/download/v${version}/${zip_file}"
+#    echo "Downloading $download_url"
+#    wget "$download_url"
+#    unzip "$zip_file" -d "$fonts_dir"
+#    rm "$zip_file"
+#done
 
 find "$fonts_dir" -name '*Windows Compatible*' -delete
 
@@ -215,19 +235,9 @@ echo "done!"
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"  >> /home/mbacon/.bashrc
 eval "$(oh-my-posh init bash --config ~/.mdb-powerlovel10k_rainbow.omp.json)" 
 
-echo "set up ssh"
-ssh-keygen -t ed25519 -C "mark.bacon@landisgyr.com"
-eval "$(ssh-agent -s)"
-ssh-add ~/.ssh/id_ed25519
-# cat ~/.ssh/id_ed25519.pub
-# add key to the git server e.g. github gitlabs
-# ssh -T git@github.com
-
-echo "config git"
-git config --global user.name "Mark Bacon"
-git config --global user.email "mark@bacon.me.uk"
 
 
 echo "###################"
 echo "SCRIPT COMPLETE!!"
 echo "###################"
+
