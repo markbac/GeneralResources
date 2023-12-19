cb000b73f35d94ddd8f02b6302e441956267f694
Mark Bacon <mark@bacon.me.uk>
2023-12-19 17:27:40 +0000

    Updates

--- Info/Docs/Documentation_structure/generate-summary.py
+++ Info/Docs/Documentation_structure/generate-summary.py
@@ -1,124 +1,106 @@
 import os
 import re
+import logging
+
+try:
+    import colorlog
+    colorlog_installed = True
+except ImportError:
+    colorlog_installed = False
+    # If colorlog is not installed, you can use regular logging without colors
+
+# Configure color logging to the terminal if colorlog is installed
+if colorlog_installed:
+    handler = colorlog.StreamHandler()
+    formatter = colorlog.ColoredFormatter(
+        '%(log_color)s%(levelname)s:%(message)s',
+        log_colors={
+            'INFO': 'white',
+            'ERROR': 'red',
+        }
+    )
+    handler.setFormatter(formatter)
+
+    logger = colorlog.getLogger(__name__)
+    logger.addHandler(handler)
+    logger.setLevel(logging.INFO)
+else:
+    # Use regular logging without colors if colorlog is not installed
+    logging.basicConfig(level=logging.INFO)
+    logger = logging.getLogger(__name__)
 
 def is_valid_heading(text):
-    if len(text) >= 3 and text.startswith('# '):
-        return True
-    else:
-        return False
-    
+    return len(text) >= 3 and text.startswith('# ')
+
 def strip_after_last_slash(string):
     last_slash_index = string.rfind("/")
     if last_slash_index == -1:
         return string
     else:
-        return string[last_slash_index+1:]
-    
+        return string[last_slash_index + 1:]
 
-    
 def generate_summary_markdown(directory, output_file):
-    
-
     with open(output_file, 'w') as f:
-        # Initialize the main description variable outside the conditional statement
         main_description = None
 
         for root, dirs, files in os.walk(directory):
-            print ('root',root,'dirs',dirs,'files',files)
             dirs.sort()
-            
-            # Skip hidden directories by filtering out directories that start with '.'
-            #if not any(subdirectory.startswith('.') for subdirectory in root.split('/')):
-            if  any(re.match('.*\\..*',subdirectory) for subdirectory in root.split('/')):                
-                # Check if it's a sub-directory
-                if root.replace(directory, '').count('/') == 1:
-                    # Create a heading for the sub-directory
-                    #print('create:',root.rfind('/'))
 
+            if any(re.match('.*\\..*', subdirectory) for subdirectory in root.split('/')):
+                if root.replace(directory, '').count('/') == 1:
                     f.write(f"# {root[root.rfind('/')+1:]} \n\n")
 
-   
                 level = root.replace(directory, '').count('/')
-                if(level >0):
-                    indent = ' ' * ((level -1) * 4)
-                    
+                if level > 0:
+                    indent = ' ' * ((level - 1) * 4)
+
                     if root.replace(directory, '').count('/') < 1:
-                        # Create a heading for the sub-directory
                         f.write('# {} \n\n'.format(os.path.basename(root)))
-                        
-                    #print("No .md files found in: ",root, 'count:', root.replace(directory, '').count('/'))
+
                     link_description = strip_after_last_slash(root)
                     link_filename = '{}.md'.format(link_description.lower())
                     link_path = os.path.join(root, link_filename)
                     link = '[{}](<{}>)'.format(link_description.lower(), link_path)
 
-                    #print("Creating link for empty directory:", link)
                     if not ('/media/' in link) and not ('/images/' in link):
                         f.write('{}- {}\n'.format(indent, link))
-                
-                if any(filename.endswith(".md")  for filename in files):
-                    # There is at least one .md file in the list, so enter the for loop
-                    
+                        logger.info(f'Added link: {link}')
+
+                if any(filename.endswith(".md") for filename in files):
                     for filename in sorted(files):
-                        if not filename.endswith('.md'):
+                        if not filename.endswith('.md') or filename.startswith('.') or "\\" in filename:
                             continue
-                        
-                        if filename.startswith('.'):
-                            continue
-                        
-                        if "\\" in filename:
-                            continue 
-                        
-                        if('/media/' in os.path.join(root, filename)):
-                            continue
-                        
-                        if('/images/' in os.path.join(root, filename)):
-                            continue
-                        
-                        #print('filename:',filename)
-                        
-                        if len(files) == 0:
-                            print('directory is empty')
 
                         filepath = os.path.join(root, filename)
-                        file_text = open(filepath, 'r').readline()
-                        file_text = file_text.strip()
+                        file_text = open(filepath, 'r').readline().strip()
 
-                        # Check if the first line is a valid heading
                         if is_valid_heading(file_text):
-                            # Explicitly reset the main description variable to None
                             main_description = None
-                            # Check if the main description variable has a value
+
                             if main_description is not None:
-                                # Use the previously assigned value
                                 link_description = main_description
                             else:
-                                # Extract the main description from the heading text
-                                heading_text_split = file_text.split(' ', 1)  # Split the heading text at the first whitespace character
-                                heading_text_main = heading_text_split[1]  # Access the main part of the heading text (excluding any leading or trailing whitespace characters)
-                                main_description = heading_text_main.strip()  # Remove any leading or trailing whitespace characters from the main heading text
-
-                                # Remove the leading '#' symbol and the trailing space
+                                heading_text_split = file_text.split(' ', 1)
+                                heading_text_main = heading_text_split[1]
+                                main_description = heading_text_main.strip()
                                 link_description = main_description.rstrip()
-                                
-                                # Create the link
-                                link = '[{}](<{}>)'.format(link_description, filepath)
 
-                            # Indent the link based on the depth of the directory
+                            link = '[{}](<{}>)'.format(link_description, filepath)
                             level = root.replace(directory, '').count('/')
-                            #print ('level: ',level)
-                            indent = ' ' * ((level ) * 4)
+                            indent = ' ' * (level * 4)
 
                             if is_valid_heading(file_text):
                                 f.write('{}- {}\n'.format(indent, link))
-
-
-
-
-
-
+                                logger.info(f'Added link: {link}')
 
 if __name__ == '__main__':
     directory = '.'
     output_file = 'SUMMARY.md'
-    generate_summary_markdown(directory, output_file)
+
+    logger.info('Started the script.')
+
+    try:
+        generate_summary_markdown(directory, output_file)
+        logger.info('Finished the script successfully.')
+    except Exception as e:
+        logger.error(f'Error in the script: {str(e)}')
